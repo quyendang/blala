20bb17771ee2a4cba4921d28cc718e009310cbbd
Quyên <11520320@gm.uit.edu.vn>
2023-01-01 23:28:28 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ pushoverAPIToken = "ah2hby41xn2viu41syq295ipeoss4e"
 pushoverUserID = "uqyjaksy71vin1ftoafoujqqg1s8rz"
 
 
-
+print("start")
 headers = {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/10.0; Trident/6.0)'}
 page = requests.get('https://googleads.g.doubleclick.net/mads/static/mad/sdk/native/sdk-core-v40-loader.appcache', headers=headers)
 htmlstr = page.text
